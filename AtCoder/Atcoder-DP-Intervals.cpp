4f5c3d1ddbd3a6c5cbaf3ed07d1972d4437b04d9
Nayeemul Islam Swad <drswad2013@gmail.com>
2020-04-05 20:52:08 +0600

    Update Atcoder-DP-Intervals.cpp

--- AtCoder/Atcoder-DP-Intervals.cpp
+++ AtCoder/Atcoder-DP-Intervals.cpp
@@ -2,7 +2,29 @@
 	Author: Nayeemul Islam Swad
 
 	Idea:
-		-
+		- Let dp[i] be the maximum answer if we only consider the intervals
+		  (l, r, a) with i <= l. Our final answer would be dp[1].
+
+		- To calculate dp[i], let's iterate over the possible positions j of the
+		  first occurrence of a set bit and take maximum among them. To handle
+		  the case when there's no set bit in [i, n], dp[i] should be at least 0.
+
+		- Now, we need to find dp[i] once we fix j. Its actually
+		  (dp[j + 1] + contribution(j)), where contribution(j) = sum of a_k such
+		  that j lies in the interval (l_k, r_k, a_k) and i <= l_k.
+
+		- To quickly find the maximum among all such positions for j and quickly
+		  calculate contribution(j), we can use lazy segment tree. We'll maintain
+		  lz[j] = dp[j] + contribution(j - 1) using the lazy segment tree.
+
+		- We'll iterate over i in decreasing order starting at n. When calculating
+		  dp[i] notice that contribution(j) remains almost the same as it was when
+		  we calculated dp[i + 1], it might only change if some intervals starting
+		  at i covers j. So we should iterate over the intervals (l_k, r_k, a_k)
+		  such that l_k = i and add a_k to lz[i + 1 ... r_k + 1] using the lazy
+		  segment tree. Then we can query the segment tree for
+		  dp[i] = query(i + 1... n + 1) and set lz[i] = dp[i] in the segment tree.
+
 */
 
 #include <bits/stdc++.h>
